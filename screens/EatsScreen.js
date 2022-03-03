b40945f26f589b25292c6a80a24ca19e2c133c14
Bryan Dang <bryanvdang@gmail.com>
2022-03-02 22:31:19 -0700

    resolved navigation issue by removing homepage component from app.js file

--- screens/EatsScreen.js
+++ screens/EatsScreen.js
@@ -1,11 +1,11 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, SafeAreaView } from "react-native";
 import React from "react";
 
 const EatsScreen = () => {
   return (
-    <View>
+    <SafeAreaView>
       <Text>EatsScreen</Text>
-    </View>
+    </SafeAreaView>
   );
 };
 
